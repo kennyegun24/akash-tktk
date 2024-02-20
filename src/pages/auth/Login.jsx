a51c80c9ee157e00e6575e37e3d848bd83c277f6
Kennyegun24 <kennyegun240@gmail.com>
2024-02-20 12:58:03 +0100

    mod

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
       const response = await axios.get("http://localhost:4000/login");
       window.location.href = `${response.data.url}`;
     } catch (error) {
-      alert("SOMETHING WENT WRONG");
+      console.log(error);
     }
   };
   return (
